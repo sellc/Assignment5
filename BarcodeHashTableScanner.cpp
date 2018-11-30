7691cf7dc8dc0996a4aa3d5ade2474928e333fde
sellc <chris.d.sellers@gmail.com>
2018-11-30 12:02:14 -0800

    Formatted for consistency
    Updated a few lines for consistency

--- BarcodeHashTableScanner.cpp
+++ BarcodeHashTableScanner.cpp
@@ -45,14 +45,14 @@ int main(int argc, char** arga) {
 	}
 	inputFile.close();
 
-		//Search for a node and print the total time
-		clock_t t = clock();
-		cout << h.search(arga[2]) << endl;
-		t = clock() - t;
+	//Search for a node and print the total time
+	clock_t t = clock();
+	cout << h.search(arga[2]) << endl;
+	t = clock() - t;
 
-		cout << "time: " << t << " milliseconds" << endl;
-		cout << CLOCKS_PER_SEC << " clocks per second" << endl;
-		cout << "time: " << t * 1.0 / CLOCKS_PER_SEC << " seconds" << endl;
+	cout << "time: " << t << " milliseconds" << endl;
+	cout << CLOCKS_PER_SEC << " clocks per second" << endl;
+	cout << "time: " << t * 1.0 / CLOCKS_PER_SEC << " seconds" << endl;
 
 	return 0;
 }
